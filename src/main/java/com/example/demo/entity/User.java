d624bb2d654f2ab0795f7a4c6b321608c737626c
김한주 <s24043@gsm.hs.kr>
2024-08-21 17:26:01 +0900

    login.html SecurityConfig.java User.java UserService.java build.gradle application.properties AuthController.java

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -5,51 +5,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class User {
 
     @Id
-    // 자동으로 증가
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String username;
-    private String password;
     private String email;
-
-    //id 값을 읽을 수 있도록 한다.
-    public Long getId() {
-        return id;
-    }
-
-    //id 값을 변경할 수 있도록 한다.
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    private String password;
 
 }
